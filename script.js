30dbb602d3042b7b7090a6c274e0c57120e5a11b
Johnny <exmple@email.com>
2021-11-07 00:10:56 +0200

    First task

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 "use strict";
 
-// let incr = 10,
-//     decr = 10;
+let numberOfFilms = prompt('Сколько фильмоы вы уже посмотрели?', '');
 
-//     ++incr;
-//     --decr;
-//     console.log(incr);
-//     console.log(decr);
+let personalMovieDB = {};
+personalMovieDB.count = numberOfFilms;
+personalMovieDB.movies = {};
+personalMovieDB.actors = {};
+personalMovieDB.genres = [];
+personalMovieDB.privat = false;
 
-    console.log(2+2*2 == '8');
-
-//     let isChecked = false,
-//         isClose = false;
-// console.log(isChecked || !isClose);
+let lastFilm = prompt('Один из последних фильмов', '');
+let countOfFilm = +prompt('На сколько вы его оцените?', '');
+let lastFilm2 = prompt('Один из последних фильмов', '');
+let countOfFilm2 = +prompt('На сколько вы его оцените?', '');
+personalMovieDB.movies[lastFilm] = countOfFilm;
+personalMovieDB.movies[lastFilm2] = countOfFilm2;
